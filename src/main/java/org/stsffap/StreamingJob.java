be953aedf1f436ab7e1a57151f4140dd2e38a73d
Till Rohrmann <trohrmann@apache.org>
2020-11-23 18:22:49 +0100

    Job where the iteration does not terminate because the sub task did not receive input

--- src/main/java/org/stsffap/StreamingJob.java
+++ src/main/java/org/stsffap/StreamingJob.java
@@ -21,25 +21,34 @@ package org.stsffap;
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.ExecutionOptions;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.configuration.WebOptions;
 import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.SimpleStreamFormat;
+import org.apache.flink.connector.file.src.reader.StreamFormat;
 import org.apache.flink.connector.file.src.reader.TextLineFormat;
+import org.apache.flink.core.fs.FSDataInputStream;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.IterativeStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.BasePathBucketAssigner;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Duration;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -58,42 +67,156 @@ public class StreamingJob {
 	public static void main(String[] args) throws Exception {
 		final ParameterTool parameterTool = ParameterTool.fromArgs(args);
 		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		final Configuration conf = new Configuration();
+		conf.setInteger(RestOptions.PORT, 8081);
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+		env.setParallelism(5);
 		final Configuration configuration = new Configuration();
-		configuration.set(ExecutionOptions.RUNTIME_MODE, RuntimeExecutionMode.valueOf(parameterTool.get("mode", "BATCH")));
+		configuration.set(ExecutionOptions.RUNTIME_MODE, RuntimeExecutionMode.valueOf(parameterTool.get("mode", "STREAMING")));
 		env.configure(configuration, StreamingJob.class.getClassLoader());
-
-		env.setParallelism(10);
 
 		final String inputFilename = parameterTool.get("input", "test-input/input");
 		final String outputFilename = parameterTool.get("output", "test-output");
-		final FileSource<String> source = FileSource
-				.forRecordStreamFormat(new TextLineFormat(), new Path(inputFilename))
+		final FileSource<Event> source = FileSource
+				.forRecordStreamFormat(new EventStreamFormat(), new Path(inputFilename))
 				.build();
 
-		final DataStream<String> input = env.fromSource(source, WatermarkStrategy.noWatermarks(), "foobar");
+		final DataStream<Event> input = env.fromSource(
+				source,
+				WatermarkStrategy
+					.<Event>forMonotonousTimestamps()
+					.withTimestampAssigner((event, timestamp) -> event.getTimestamp()),
+				"foobar");
+//		final DataStream<String> input = env.fromElements("ab c", "d e f", "foobar barfoo");
 
-		final DataStream<String> words = input.flatMap((FlatMapFunction<String, String>) (s, collector) -> {
-			for (String split : s.split(" ")) {
+		final DataStream<String> words = input.flatMap((FlatMapFunction<Event, String>) (s, collector) -> {
+			for (String split : s.getValue().split(" ")) {
 				collector.collect(split.trim());
 			}
 		}).returns(String.class);
 
-		final DataStream<Tuple2<String, Integer>> wordsWithSingleCount = words.rescale().map(i -> i).setParallelism(20)
-				.map((MapFunction<String, Tuple2<String, Integer>>) word -> Tuple2.of(word, 1))
-				.returns(TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {}));
+		final SingleOutputStreamOperator<DataObject> process = words.rebalance().keyBy(x -> x).process(new KeyedProcessFunction<String, String, DataObject>() {
+			@Override
+			public void processElement(String value, Context ctx, Collector<DataObject> out) throws Exception {
+				ctx.timerService().registerProcessingTimeTimer(ctx.timestamp() + 10);
+				out.collect(new DataObject(
+						value,
+						ctx.timerService().currentWatermark(),
+						ctx.timerService().currentProcessingTime()));
+			}
 
-		final SingleOutputStreamOperator<Tuple2<String, Integer>> wordsWithCounts = wordsWithSingleCount
-				.keyBy(t -> t.f0)
-				.reduce((ReduceFunction<Tuple2<String, Integer>>) (stringIntegerPair, t1) -> Tuple2.of(stringIntegerPair.f0, stringIntegerPair.f1 + t1.f1));
+			@Override
+			public void onTimer(long timestamp, OnTimerContext ctx, Collector<DataObject> out) throws Exception {
+				out.collect(new DataObject("timer", timestamp, timestamp));
+			}
+		});
 
-		final FileSink<Tuple2<String, Integer>> sink = FileSink.<Tuple2<String, Integer>>forRowFormat(new Path(outputFilename), new SimpleStringEncoder<>())
+		final IterativeStream<DataObject> iterate = process.iterate();
+
+		final OutputTag<DataObject> sideOutputTag = new OutputTag<DataObject>("sideOutput") {};
+		final SingleOutputStreamOperator<DataObject> iterationStep = iterate.map(x -> x);
+		final DataStream<DataObject> sideOutput = iterationStep.getSideOutput(sideOutputTag);
+
+		iterate.closeWith(iterationStep);
+
+		final FileSink<DataObject> sink = FileSink.<DataObject>forRowFormat(new Path(outputFilename), new SimpleStringEncoder<>())
 				.withBucketAssigner(new DateTimeBucketAssigner<>())
 				.build();
 
-		wordsWithCounts.sinkTo(sink);
+		sideOutput.sinkTo(sink);
 
 		// execute program
 		env.execute("Flink Batch streaming job");
+
+		System.out.println("foobar");
+	}
+
+	public static final class EventStreamFormat extends SimpleStreamFormat<Event> {
+		@Override
+		public Reader<Event> createReader(Configuration config, FSDataInputStream stream) throws IOException {
+			return new EventReader(new BufferedReader(new InputStreamReader(stream)));
+		}
+
+		@Override
+		public TypeInformation<Event> getProducedType() {
+			return TypeInformation.of(new TypeHint<Event>() {});
+		}
+	}
+
+	public static final class EventReader implements StreamFormat.Reader<Event> {
+		private final BufferedReader reader;
+
+		public EventReader(BufferedReader reader) {
+			this.reader = reader;
+		}
+
+		@Override
+		public Event read() throws IOException {
+			final String line = reader.readLine();
+
+			if (line != null) {
+				final String[] split = line.split(";");
+
+				return new Event(Long.parseLong(split[0]), split[1]);
+			} else {
+				return null;
+			}
+		}
+
+		@Override
+		public void close() throws IOException {
+			reader.close();
+		}
+	}
+
+	public static final class Event {
+		private final long timestamp;
+		private final String value;
+
+		public Event(long timestamp, String value) {
+			this.timestamp = timestamp;
+			this.value = value;
+		}
+
+		public long getTimestamp() {
+			return timestamp;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+
+	public static final class DataObject {
+		private final String value;
+		private final long watermark;
+		private final long processingTime;
+
+		public DataObject(String value, long watermark, long processingTime) {
+			this.value = value;
+			this.watermark = watermark;
+			this.processingTime = processingTime;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public long getWatermark() {
+			return watermark;
+		}
+
+		public long getProcessingTime() {
+			return processingTime;
+		}
+
+		@Override
+		public String toString() {
+			return "DataObject{" +
+					"value='" + value + '\'' +
+					", watermark=" + watermark +
+					", processingTime=" + processingTime +
+					'}';
+		}
 	}
 }
